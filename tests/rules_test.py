a1204628f8d81168c7e9b6729edf7b84e2dadb4d
Tommy Praeger <tommypraeger@gmail.com>
2024-05-12 07:18:31 -0400

    add test for initialization of Rule

--- tests/rules_test.py
+++ tests/rules_test.py
@@ -2,7 +2,7 @@
 Test rules. Rules are used to filter or alter the messages DataFrame.
 """
 
-from typing import Any
+from typing import Any, Callable
 
 import pandas as pd
 import pytest
@@ -12,8 +12,35 @@ from textsgpt import rules
 from .utils import assert_dataframes_equal
 
 
-# TODO
-# test initialization of Rule class
+def example_rule_func(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    No-op function used to test rule initialization.
+    """
+    return df
+
+
+@pytest.mark.parametrize(
+    "rule,expected_func,expected_kwargs",
+    [
+        (rules.Rule(example_rule_func), example_rule_func, {}),
+        (
+            rules.Rule(example_rule_func, {"key": "value"}),
+            example_rule_func,
+            {"key": "value"},
+        ),
+    ],
+)
+def test_rule_initialization(
+    rule: rules.Rule,
+    expected_func: Callable[..., pd.DataFrame],
+    expected_kwargs: dict[str, Any],
+):
+    """
+    Test that a rule can be initialized as expected.
+    Includes cases for when kwargs is left empty and when kwargs is passed.
+    """
+    assert rule.func == expected_func
+    assert rule.kwargs == expected_kwargs
 
 
 @pytest.mark.parametrize(
